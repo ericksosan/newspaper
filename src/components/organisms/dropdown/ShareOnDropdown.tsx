758e66ce4dfa36c84218c5a5b89ab98d31974509
Erick Sosa <92877736+ErickDev03@users.noreply.github.com>
2023-09-12 16:38:12 -0400

    feat(dynamic news): added icons (#9)

--- src/components/organisms/dropdown/ShareOnDropdown.tsx
+++ src/components/organisms/dropdown/ShareOnDropdown.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { FacebookShareButton, TwitterShareButton, LinkedinShareButton, WhatsappShareButton } from 'react-share'
-import { twMerge } from 'tailwind-merge'
 import { Button } from '../../atoms'
+import { FacebookIcon, LinkedInIcon, WhatsappIcon, XTwitterIcon } from '../../atoms/icon'
+import { useClickAway } from '@uidotdev/usehooks'
 
 interface ShareOnDropdownProps {
   id: string
@@ -11,65 +12,70 @@ interface ShareOnDropdownProps {
 
 export const ShareOnDropdown: React.FC<ShareOnDropdownProps> = ({ id, title, summary }) => {
   const [dropdownToggle, setDropdownToggle] = useState<boolean>(false)
+  const ref = useClickAway<HTMLUListElement>(() => { setDropdownToggle(!dropdownToggle) })
 
   return (
-    <div className="flex gap-2 items-center relative">
+    <div className="flex gap-2 items-center relative w-auto">
       <Button
+        className='py-1.5 sm:py-2.5'
         colors='blue'
-        onClick={() => { setDropdownToggle(!dropdownToggle) }}
-        >
+        onClick={() => { setDropdownToggle(true) }}>
         Share on
       </Button>
 
-      <ul className={
-        twMerge(
-          `flex-col gap-2 rounded-md bg-white border dark:border-slate-700
-          overflow-hidden top-12 dark:bg-slate-800 px-2.5 py-3 sm:w-full xl:w-auto absolute
-          left-0  dark:text-white text-slate-900 hover:[&>li]:text-azure-radiance-700
-        dark:hover:[&>li]:text-white [&>li]:font-medium hover:[&>li]:hover:bg-slate-100
-          [&>li]:w-full [&>li]:rounded-md dark:hover:[&>li]:bg-slate-700 [&>li]:p-2 [&>li]:text-lg`,
-          dropdownToggle ? 'flex' : 'hidden'
-        )
-      }>
+      {
+        dropdownToggle &&
+        <ul
+          ref={ref}
+          className='flex-col gap-2 rounded-md bg-white border dark:border-slate-700
+          overflow-hidden top-12 dark:bg-slate-800 px-2.5 py-3 absolute sm:right-0
+          dark:text-white text-slate-900 hover:[&>li]:text-azure-radiance-700
+          hover:[&>li]:fill-azure-radiance-700 dark:hover:[&>li]:text-white
+          dark:[&>li]:fill-white font-medium hover:[&>li]:bg-slate-100 [&>li]:w-full
+          [&>li]:rounded-md dark:hover:[&>li]:bg-slate-700 [&>li]:p-2 [&>li]:text-lg'>
+          <li>
+            <TwitterShareButton
+              className='flex gap-x-2 items-center w-full h-full'
+              title={title}
+              url={`http://localhost:5173/new/${id}`}>
+              <XTwitterIcon />
+              X
+            </TwitterShareButton>
+          </li>
 
-        <li>
-          <FacebookShareButton
-            className='block w-full h-full'
-            title={title}
-            url={`http://localhost:5173/new/${id}`}>
-            Facebook
-          </FacebookShareButton>
-        </li>
+          <li>
+            <FacebookShareButton
+              className='flex gap-x-2 items-center w-full h-full'
+              title={title}
+              url={`http://localhost:5173/new/${id}`}>
+              <FacebookIcon />
+              Facebook
+            </FacebookShareButton>
+          </li>
 
-        <li>
-          <WhatsappShareButton
-            url={`http://localhost:5173/new/${id}`}
-            className=' block w-full h-full'
-            title={title}>
-            Whatsapp
-          </WhatsappShareButton>
-        </li>
+          <li>
+            <WhatsappShareButton
+              url={`http://localhost:5173/new/${id}`}
+              className=' flex gap-x-2 items-center w-full h-full'
+              title={title}>
+              <WhatsappIcon />
+              Whatsapp
+            </WhatsappShareButton>
+          </li>
 
-        <li>
-          <TwitterShareButton
-            className='block w-full h-full'
-            title={title}
-            url={`http://localhost:5173/new/${id}`}>
-            X
-          </TwitterShareButton>
-        </li>
-
-        <li>
-          <LinkedinShareButton
-            className=' block w-full h-full'
-            title={title}
-            summary={summary}
-            source='http://localhost:5173/'
-            url={`http://localhost:5173/new/${id}`}>
-            LinkedIn
-          </LinkedinShareButton>
-        </li>
-      </ul>
+          <li>
+            <LinkedinShareButton
+              className=' flex gap-x-2 items-center w-full h-full'
+              title={title}
+              summary={summary}
+              source='http://localhost:5173/'
+              url={`http://localhost:5173/new/${id}`}>
+              <LinkedInIcon />
+              LinkedIn
+            </LinkedinShareButton>
+          </li>
+        </ul>
+      }
     </div>
   )
 }
